6b4094aee72edae813d5303e7fa64172d9f9683a
elemidiv <angel.emiliano.dv@gmail.com>
2026-09-06 23:36:55 -0600

    2.1. La primera ventana
    +Se crea el proyecto de Unity
    +Se agregan los puntos correspondientes al UI para act 2.1

---
+++ Aplicacion Interactiva/Assets/Scripts/Links.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Links : MonoBehaviour
+{
+    public void Link1()
+    {
+        Application.OpenURL("https://licenciaturas.udgplus.udg.mx");
+    }
+
+    public void Link2()
+    {
+        Application.OpenURL("https://www.youtube.com/");
+    }
+
+    public void Link3()
+    {
+        Application.OpenURL("https://unity.com/es");
+    }
+}
